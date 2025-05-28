8bfd32a154ea6e092750ae1bbb39638d3431c682
Jacob Roelofsz <87379740+jroelofsz@users.noreply.github.com>
2025-05-27 21:56:58 -0500

    updated config base for deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/static_site/', // <-- Add this line (replace with your repo name if different)
+  base: '/', // <-- Add this line (replace with your repo name if different)
   plugins: [react()],
 })
