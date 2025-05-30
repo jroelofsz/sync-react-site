b06be249d0cfde3fc9c0f765687db71334032036
Jacob Roelofsz <87379740+jroelofsz@users.noreply.github.com>
2025-05-30 00:34:56 -0500

    overhauled about page section for devs, TODO replicate across entire page, make it a component?

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,25 +2,44 @@ export default function About() {
     return (
         <div className="container text-white text-center">
             <h1 className='page-header'>About</h1>
-
             <div className="container">
-                <hr />
-                <h2 className='p-1'>Developers</h2>
-                <hr />
-                <a href='/' rel='noopener noreferrer' className='text-decoration-none text-white logo-link'>
-                    <img src='../images/lbs_scripts_logo_icon_96x96.png' alt="Lebubble Logo" className="rounded-circle mb-4" style={{ width: '96px', height: '96px' }} />
-                </a>
-                <h2>lebubble</h2>
-                <br />
+                <h2 className='p-1'>Lebubble Scripts</h2>
                 <p>
-                    I'm a web developer who loves turning ideas into real digital experiences. I started out with small projects that eventually sparked a passion for building things that not only work smoothly but also feel personal. I work a lot with React, TypeScript, and full-stack development, always aiming for solutions that are practical, scalable, and friendly to users.
-                    <br /><br />
-                    I've spent years learning how to design clean, intuitive interfaces and build robust systems that operate seamlessly behind the scenes. This journey has taught me to appreciate the blend of art and logic in coding – solving complex problems with elegant solutions.
-                    <br /><br />
-                    My goal is to create digital experiences that meet real needs while connecting with people on a human level. Every line of code is a step towards making that vision a reality.
-                    <br /><br />
-                    <em style={{ fontSize: '0.8rem' }}>lebubble</em>
+                    Thank you for checking us out! Below is a brief overview of who we are and what we do. We're excited to share our journey with you and hope you find our work as interesting as we do.
                 </p>
+                <br />
+                <hr />
+                <h2 className='p-1 about-section'>Developers</h2>
+                <hr />
+
+                <div className="row">
+                    <div className="col-md-6 d-inline">
+                        <h2>lebubble</h2>
+                        <a href='/' rel='noopener noreferrer' className='text-decoration-none text-white'>
+                            <img src='../images/lbs_scripts_logo_icon_96x96.png' alt="Lebubble Logo" className="rounded-circle mb-4" style={{ width: '96px', height: '96px' }} />
+                        </a>
+                        <a href='https://github.com/Project-Sloth' target="_blank" rel="noopener noreferrer" className='text-decoration-none'>
+                            <img src='../images/project_sloth_logo.png' alt='Project Sloth Logo' className="rounded-circle mb-4" style={{ width: '96px', height: '96px' }} />
+                        </a>
+                    </div>
+                    <div className="col-md-6">
+                        <p>
+                            I'm a web developer who loves turning ideas into real digital experiences. I started out with small projects that eventually sparked a passion for building things that not only work smoothly but also feel personal. I work a lot with React, TypeScript, and full-stack development, always aiming for solutions that are practical, scalable, and friendly to users.
+                            <br /><br />
+                            I've spent years learning how to design clean, intuitive interfaces and build robust systems that operate seamlessly behind the scenes. This journey has taught me to appreciate the blend of art and logic in coding – solving complex problems with elegant solutions.
+                            <br /><br />
+                            My goal is to create digital experiences that meet real needs while connecting with people on a human level. Every line of code is a step towards making that vision a reality.
+                            <br /><br />
+                            My current focus in on developing custom scripts and contributing to Project Sloth.
+                            <br /><br />
+
+                            <br /><br />
+                            <em style={{ fontSize: '0.8rem' }}>lebubble</em>
+                        </p>
+                    </div>
+                    <br />
+                </div>
+                
                 <hr />
                 <h2 className='p-1'>Partners</h2>
                 <hr />
