1e06a04348042d7294d773972b0d2a397ccef894
Jacob Roelofsz <87379740+jroelofsz@users.noreply.github.com>
2025-05-30 18:00:51 -0500

    staging github pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/', // <-- Add this line (replace with your repo name if different)
+  base: '/react-site', 
   plugins: [react()],
+  build: {
+    outDir: 'docs'
+  }
 })
