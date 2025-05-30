3a327b6e490f371cab4b955870e51daf66c3bb17
Jacob Roelofsz <87379740+jroelofsz@users.noreply.github.com>
2025-05-30 17:55:51 -0500

    overhauled about page, used rows instead of containers

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -39,22 +39,32 @@ export default function About() {
                     </div>
                     <br />
                 </div>
-                
-                <hr />
-                <h2 className='p-1'>Partners</h2>
-                <hr />
-                <a href='google.com' target='_blank' rel='noopener noreferrer' className='text-decoration-none text-white logo-link'>
-                    <img src="../images/kxirby_logo_96x96.png" alt='kxirby logo' className="rounded-circle mb-4" style={{ width: '96px', height: '96px' }} />
-                </a>
-                <h2>kxirby</h2>
+                <div>
+                    <hr />
+                    <h2 className='p-1 about-section'>Partners</h2>
+                    <hr />
+                </div>
+                <div className='row'>
+                    <div className='col-md-6 d-inline'>
+                        <h2>kxirby</h2>
+                        <a href='google.com' target='_blank' rel='noopener noreferrer' className='text-decoration-none text-white'>
+                            <img src="../images/kxirby_logo_96x96.png" alt='kxirby logo' className="rounded-circle mb-4" style={{ width: '96px', height: '96px' }} />
+                        </a>
+                    </div>
+                    <div className='col-md-6'>
+                        <p>
+                            Hey, I’m the owner of Sync RP, the main server that this dev team works on. I’ve been playing FiveM for a long time now and have always had a deep love for video games and online communities. Over the years, I’ve owned and managed multiple successful and populated servers, which has given me a good grip on what works and what doesn’t when it comes to creating a stable and enjoyable RP experience. Sync RP is my current focus, and it's where I’m putting all my energy into building something long-term and community-driven.
+                            <br /><br />
+                            Outside of running Sync, I also help other people with server setups—from full builds to troubleshooting, resource optimization, and helping get new communities off the ground. I’ve worked with a lot of server owners behind the scenes, helping them get things running right. On top of that, I’m also involved with content creation and help stream and promote FiveM content, both for Sync and other projects. Whether it’s community building, development, or just boosting visibility, I try to be hands-on wherever I can add value.
+                            <br /><br />
+                            <em style={{ fontSize: '0.8rem' }}>kxirby</em>
+                        </p>
+                    </div>
+                </div>
+
+
                 <br />
-                <p>
-                    Hey, I’m the owner of Sync RP, the main server that this dev team works on. I’ve been playing FiveM for a long time now and have always had a deep love for video games and online communities. Over the years, I’ve owned and managed multiple successful and populated servers, which has given me a good grip on what works and what doesn’t when it comes to creating a stable and enjoyable RP experience. Sync RP is my current focus, and it's where I’m putting all my energy into building something long-term and community-driven.
-                    <br /><br />
-                    Outside of running Sync, I also help other people with server setups—from full builds to troubleshooting, resource optimization, and helping get new communities off the ground. I’ve worked with a lot of server owners behind the scenes, helping them get things running right. On top of that, I’m also involved with content creation and help stream and promote FiveM content, both for Sync and other projects. Whether it’s community building, development, or just boosting visibility, I try to be hands-on wherever I can add value.
-                    <br /><br />
-                    <em style={{ fontSize: '0.8rem' }}>kxirby</em>
-                </p>
+
             </div>
             <hr />
         </div>
