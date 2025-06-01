42df009d70c7c57f95e40c7ac51c9907fc48fb19
Jacob Roelofsz <87379740+jroelofsz@users.noreply.github.com>
2025-06-01 18:12:58 -0500

    more config changes

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/react-site', 
+  base: '/react-site/', 
   plugins: [react()],
   build: {
     outDir: 'docs'
